7c954fa9165e0ff9c3a50935137f9121b15c40b2
Imated <lineborder122@gmail.com>
2023-01-21 20:35:38 -0500

    Save Settings + Fading

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,38 +1,52 @@
-using System;
 using UnityEngine;
 using UnityEngine.Audio;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private AudioMixerGroup musicMixerGroup;
     [SerializeField] private AudioMixerGroup sfxMixerGroup;
-    [SerializeField] private Slider volumeSlider;
+    [SerializeField] private Slider masterSlider;
     [SerializeField] private Slider musicSlider;
     [SerializeField] private Slider sfxSlider;
 
-    private void Awake()
-    {
-        volumeSlider.onValueChanged.AddListener(UpdateVolume);
-        musicSlider.onValueChanged.AddListener(UpdateVolume);
-        sfxSlider.onValueChanged.AddListener(UpdateVolume);
-    }
-
     private void Start()
     {
+        masterSlider.onValueChanged.AddListener(UpdateVolume);
+        musicSlider.onValueChanged.AddListener(UpdateVolume);
+        sfxSlider.onValueChanged.AddListener(UpdateVolume);
         UpdateVolume(0f);
+        LoadSettings();
+    }
+
+    private void OnApplicationQuit()
+    {
+        SaveSettings();
     }
 
     public void Play()
     {
-        SceneManager.LoadScene("Gameplay");
+        TransitionManager.Instance.FadeScene("Gameplay");
     }
     
     private void UpdateVolume(float newVolume)
     {
-        AudioListener.volume = volumeSlider.value;
+        AudioListener.volume = masterSlider.value;
         musicMixerGroup.audioMixer.SetFloat("Volume", musicSlider.value);
         sfxMixerGroup.audioMixer.SetFloat("Volume", sfxSlider.value);
+    }
+    
+    private void SaveSettings()
+    {
+        PlayerPrefs.SetFloat("Master Volume", masterSlider.value);
+        PlayerPrefs.SetFloat("Music Volume", musicSlider.value);
+        PlayerPrefs.SetFloat("SFX Volume", sfxSlider.value);
+    }
+    
+    private void LoadSettings()
+    {
+        masterSlider.value = PlayerPrefs.GetFloat("Master Volume", 1);
+        musicSlider.value = PlayerPrefs.GetFloat("Music Volume", -15);
+        sfxSlider.value = PlayerPrefs.GetFloat("SFX Volume", -15);
     }
 }
