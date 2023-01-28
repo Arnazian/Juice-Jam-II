394fce69b8363307945f8ebb5450562b26b8d613
Arnazian <Arnazian@gmail.com>
2023-01-27 17:49:55 -0700

    Some progress with boss

---
+++ Assets/Scripts/Bosses/BossHealthManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossHealthManager : MonoBehaviour
+{
+    [SerializeField] private int bossMaxHealth;
+    [SerializeField] private Slider bossHealthSlider;
+    [SerializeField] private int amountOfStages;
+
+    private int bossCurHealth;
+
+    private void Start()
+    {
+        bossCurHealth = bossMaxHealth;
+        bossHealthSlider.maxValue = bossMaxHealth;
+        bossHealthSlider.value = bossCurHealth; 
+    }
+    public void TakeDamage(int damage)
+    {
+        bossCurHealth -= damage;
+        bossHealthSlider.value = bossCurHealth;
+    }
+
+
+}
+    
+}
