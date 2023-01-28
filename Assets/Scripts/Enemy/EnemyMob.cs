44f1346f805fbf80eaa16427053ec90a9a2c285c
Imated <lineborder122@gmail.com>
2023-01-28 11:43:49 -0500

    Enemy Mob Health Display

--- Assets/Scripts/Enemy/EnemyMob.cs
+++ Assets/Scripts/Enemy/EnemyMob.cs
@@ -1,22 +1,19 @@
 using Pathfinding;
 using UnityEngine;
 
-public class EnemyMob : MonoBehaviour, IDamageable
+[RequireComponent(typeof(EnemyMobHealthManager))]
+public class EnemyMob : MonoBehaviour
 {
-    [SerializeField] private float maxHealth = 100;
-    
     [SerializeField] private float attackCooldown;
     [SerializeField] private float attackDamage = 10f;
     [SerializeField] private Transform player;
 
     private IAstarAI _agent;
     private bool _isAttacking;
-    private float _health;
 
     private void Awake()
     {
         _agent = GetComponent<IAstarAI>();
-        _health = maxHealth;
     }
 
     private void Update()
@@ -33,9 +30,6 @@ public class EnemyMob : MonoBehaviour, IDamageable
             _isAttacking = false;
             CancelInvoke(nameof(Attack));
         }
-        
-        if(_health <= 0)
-            Destroy(gameObject);
     }
 
     private void Attack()
@@ -43,10 +37,5 @@ public class EnemyMob : MonoBehaviour, IDamageable
         if(!_isAttacking || PauseMenu.Instance.IsPaused)
             return;
         player.GetComponent<IDamageable>().Damage(attackDamage);
-    }
-
-    public void Damage(float amount)
-    {
-        _health -= amount;
     }
 }
